5753f6611974108e21b0b6a2c204dd1b11ce2e28
Nicolas Garreau <nicolas.garreau@etu.unilim.fr>
2018-04-30 14:50:32 +0200

    créer un espace de jeu

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -9,16 +9,16 @@ public class SpaceInvaders {
 		this.longueur = longueur;
 		this.hauteur = hauteur;
 	}
-	
-	  @Override
-	    public String toString() {
-	        String espaceDeJeux = "";
-	        for (int i = 0; i < hauteur; i++) {
-	            for (int j = 0; j < longueur; j++) {
-	                espaceDeJeux += ".";
-	            }
-	            espaceDeJeux += "\n";
-	        }
-	        return espaceDeJeux;
-	    }
+
+	@Override
+	public String toString() {
+		StringBuilder espaceDeJeu = new StringBuilder();
+		for (int i = 0; i < hauteur; i++) {
+			for (int j = 0; j < longueur; j++) {
+				espaceDeJeu.append('.');
+			}
+			espaceDeJeu.append('\n');
+		}
+		return espaceDeJeu.toString();
+	}
 }
