01b6d696ef5af7087eccb12b34a48c592d98a5e7
Baptiste Paterne <baptiste.paterne@gmail.com>
2018-05-07 11:37:50 +0200

    story 4

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -2,29 +2,6 @@ package fr.unilim.iut.spaceinvaders;
 
 public class Vaisseau {
 
-
-	int x;
-	int y;
-
-	public Vaisseau(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
-	}
-
-	public void seDeplacerVersLaDroite() {
-		this.x = this.x + 1 ;
-	}
-
-
-	
-	public int getX() {
-        return this.x;
-	}
-
 }
 
 
