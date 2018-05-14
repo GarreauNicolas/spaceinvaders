4ae47150cfc7afc405d30fac3ed271559a05dac4
Nicolas Garreau <nicolas.garreau@etu.unilim.fr>
2018-05-14 10:32:03 +0200

    fonction 2

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -64,7 +64,14 @@ public class SpaceInvaders {
 	public void deplacerVaisseauVersLaGauche(){
 		if (vaisseau.abscisse() > 0) vaisseau.seDeplacerVersLaGauche();
 	}
-	
+
+	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
+		if (  !estDansEspaceJeu(x, y) )
+			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+		vaisseau = new Vaisseau(longueur,hauteur,x, y); 
+	}
+
 
 }
 
--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -5,14 +5,29 @@ public class Vaisseau {
 
 	int x;
 	int y;
+	int longueur;
+	int hauteur;
 
 	public Vaisseau(int x, int y) {
+		this.longueur=1;
+		this.hauteur=1;
+		this.x = x;
+		this.y = y;
+	}
+
+	public Vaisseau(int longueur, int hauteur, int x2, int y2) {
+		this.longueur=longueur;
+		this.hauteur=hauteur;
 		this.x = x;
 		this.y = y;
 	}
 
 	public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
+		if ((this.x<=x) && (x<=this.x+this.longueur-1)) 
+		      if ( (this.y-this.hauteur+1<=y) && (y<=this.y))
+			  return true;
+		
+	     return false;
 	}
 
 	public void seDeplacerVersLaDroite() {
