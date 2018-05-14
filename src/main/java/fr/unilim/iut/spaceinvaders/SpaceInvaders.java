d7fa7c38f9a446dcab17a97c26829e156786acbb
Baptiste Paterne <baptiste.paterne@gmail.com>
2018-05-14 10:46:08 +0200

    recup code

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -4,75 +4,7 @@ import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvaders {
 
-	private static final char MARQUE_VAISSEAU = 'V';
-	private static final char MARQUE_VIDE = '.';
-	private static final Object MARQUE_FIN_DE_LIGNE = '\n';
-	int longueur;
-	int hauteur;
-	Vaisseau vaisseau;
-	
-	public SpaceInvaders(int hauteur, int longueur){
-		this.longueur=longueur;
-		this.hauteur=hauteur;
-		
-	}
 
-	public String recupererEspaceJeuDansChaineASCII() {
-		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int y = 0; y < hauteur; y++) {
-			for (int x = 0; x < longueur; x++) {
-				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
-			}
-			espaceDeJeu.append(MARQUE_FIN_DE_LIGNE);
-		}
-		return espaceDeJeu.toString();
-	}
-
-	private char recupererMarqueDeLaPosition(int x, int y) {
-		char marque;
-		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
-			marque=MARQUE_VAISSEAU;
-		else
-			marque=MARQUE_VIDE;
-		return marque;
-	}
-
-	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
-		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
-	}
-
-	private boolean aUnVaisseau() {
-		return vaisseau!=null;
-	}
-
-	public void positionnerUnNouveauVaisseau(int x, int y) {
-		if (  !estDansEspaceJeu(x, y) )
-			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
-
-		vaisseau = new Vaisseau(x, y); 
-
-	}
-
-	private boolean estDansEspaceJeu(int x, int y) {
-		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
-	}
-
-
-	public void deplacerVaisseauVersLaDroite() {
-		if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
-	}
-
-
-	public void deplacerVaisseauVersLaGauche(){
-		if (vaisseau.abscisse() > 0) vaisseau.seDeplacerVersLaGauche();
-	}
-
-	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
-		if (!estDansEspaceJeu(x, y))
-			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
-
-		vaisseau = new Vaisseau(longueur,hauteur,x,y);
-	}
 
 
 
--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -2,43 +2,6 @@ package fr.unilim.iut.spaceinvaders;
 
 public class Vaisseau {
 
-	int x;
-	int y;
-	int longueur;
-	int hauteur;
-
-
-
-	public Vaisseau(int longueur, int hauteur, int x2, int y2) {
-		this.longueur=longueur;
-		this.hauteur=hauteur;
-		this.x = x;
-		this.y = y;
-
-	}
-	public int abscisse() {
-		return x;
-	}
-	
-	public int ordonnee() {
-		return y;
-	}
-	
-	public boolean occupeLaPosition(int x, int y) {
-		if ((this.x<=x) && (x<=this.x+this.longueur-1)) 
-		      if ((this.y-this.hauteur+1<=y) && (y<=this.y))
-			  return true;
-		
-	     return false;
-	}
-
-	public void seDeplacerVersLaDroite() {
-		this.x = this.x + 1 ;
-	}
-	public void seDeplacerVersLaGauche() {
-		this.x = this.x - 1 ;
-
-	}
 
 }
 
