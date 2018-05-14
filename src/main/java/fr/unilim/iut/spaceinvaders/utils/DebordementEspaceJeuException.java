708984481592cf5c7e350eaf3e6a925eaa85911e
Baptiste Paterne <baptiste.paterne@gmail.com>
2018-05-14 11:35:45 +0200

    reparation erreurs

--- src/main/java/fr/unilim/iut/spaceinvaders/utils/DebordementEspaceJeuException.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/utils/DebordementEspaceJeuException.java
@@ -2,15 +2,9 @@ package fr.unilim.iut.spaceinvaders.utils;
 
 @SuppressWarnings("serial")
 public class DebordementEspaceJeuException extends RuntimeException {
-<<<<<<< HEAD
+
 	public DebordementEspaceJeuException(String message) {
 		super(message);
 	}
-}
-=======
 
-    public DebordementEspaceJeuException(String message) {
-	    super(message);
-    }
 }
->>>>>>> branch 'master' of https://github.com/GarreauNicolas/spaceinvaders
